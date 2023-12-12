1514152fc57e2109416695e718f19ba64cfabc79
S00227414 <s00227414@atu.ie>
2023-12-12 22:25:27 +0000

    Issues with displaying top 5 and deleting other entries. Keeping other entries, only displaying top5

--- app/src/main/java/com/example/madgame/MainActivity.java
+++ app/src/main/java/com/example/madgame/MainActivity.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,6 +38,48 @@ public class MainActivity extends AppCompatActivity {
                 displayDataFromDatabase();
             }
         });
+        Button btnDeleteAll = findViewById(R.id.btnDeleteAll);
+        btnDeleteAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteAllEntries();
+            }
+        });
+
+    }
+
+    private void deleteLowScores(SQLiteDatabase db) {
+        String selectQuery = "SELECT * FROM HighScores ORDER BY Score DESC LIMIT 5";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int scoreColumnIndex = cursor.getColumnIndex("Score");
+            int count = cursor.getCount();
+            Log.d("TotalCount", "Total Count: " + count);
+
+            // Keep only the top five scores
+            if (count > 5) {
+                cursor.moveToPosition(4); // Move to the 5th record
+
+                // Check if the column index for "Score" is valid
+                if (scoreColumnIndex != -1) {
+                    @SuppressLint("Range") int thresholdScore = cursor.getInt(scoreColumnIndex);
+
+                    // Log the threshold score
+                    Log.d("ThresholdScore", "Threshold Score: " + thresholdScore);
+
+                    // Delete scores lower than the threshold
+                    int deletedRows = db.delete("HighScores", "Score < ?", new String[]{String.valueOf(thresholdScore)});
+                    Log.d("DeletedRows", "Deleted Rows: " + deletedRows);
+                } else {
+                    Log.d("ScoreColumnError", "Column index for 'Score' not found.");
+                }
+            }
+        }
+
+        if (cursor != null) {
+            cursor.close();
+        }
     }
 
     private void addDataToDatabase() {
@@ -44,18 +88,29 @@ public class MainActivity extends AppCompatActivity {
         values.put("PlayerName", editTextName.getText().toString());
         values.put("Score", Integer.parseInt(editTextScore.getText().toString()));
         db.insert("HighScores", null, values);
+
+        // Check and delete lower scores
+        deleteLowScores(db);
+
+        // Close the database connection after displaying the data
+        displayDataFromDatabase();
         db.close();
     }
 
+
+
+
+
     private void displayDataFromDatabase() {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM HighScores ORDER BY Score DESC", null);
-        StringBuilder displayText = new StringBuilder("Player Data:\n");
+        Cursor cursor = db.rawQuery("SELECT * FROM HighScores ORDER BY Score DESC LIMIT 5", null);
+        StringBuilder displayText = new StringBuilder("Top Five Scores:\n");
+
 
         if (cursor.moveToFirst()) {
             do {
-                @SuppressLint("Range") String playerName = cursor.getString(cursor.getColumnIndex("PlayerName"));
-                @SuppressLint("Range") int score = cursor.getInt(cursor.getColumnIndex("Score"));
+                String playerName = cursor.getString(cursor.getColumnIndex("PlayerName"));
+                int score = cursor.getInt(cursor.getColumnIndex("Score"));
                 displayText.append("Name: ").append(playerName).append(", Score: ").append(score).append("\n");
             } while (cursor.moveToNext());
         }
@@ -65,10 +120,20 @@ public class MainActivity extends AppCompatActivity {
 
         textViewDisplay.setText(displayText.toString());
     }
+    private void deleteAllEntries() {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db.delete("HighScores", null, null); // Deletes all entries from the table
+        db.close();
+
+        // Refresh displayed data after deletion
+        displayDataFromDatabase();
+    }
+
 
     @Override
     protected void onDestroy() {
         dbHelper.close();
         super.onDestroy();
     }
+
 }
